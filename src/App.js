ed9de93971295f7f344066edf830a2d328f2fb17
Gabriel Bonato Carneiro <gabrielbonatocarneiro@gmail.com>
2022-01-11 07:54:21 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import Tmdb from './Tmdb'
+import MovieRow from './components/MovieRow'
+import FeaturedMovie from './components/FeaturedMovie'
 
-function App() {
+export default () => {
+  const [movieList, setMovieList] = useState([])
+
+  useEffect(() => {
+    const loadAll = async () => {
+      // Pegando a lista total
+      let list = await Tmdb.getHomeList()
+      setMovieList(list)
+    }
+
+    loadAll()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='page'>
+      <FeaturedMovie />
 
-export default App;
+      <section className='lists'>
+        {movieList.map((item, index) => (
+          <MovieRow key={index} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
+  )
+}
---
+++ src/Tmdb.js
@@ -0,0 +1,68 @@
+const API_KEY = 'cd540e7abe183a64c53a919a3725b941'
+const API_BASE = 'https://api.themoviedb.org/3'
+
+/*
+- originais da netflix
+- recomendados (trending)
+- em alta (top rated)
+- ação
+- comédia
+- terror
+- romance
+- documentários
+*/
+
+const basicFetch = async (endpoint) => {
+  const req = await fetch(
+    `${API_BASE}${endpoint}language=pt-BR&api_key=${API_KEY}`
+  )
+  const json = await req.json()
+  return json
+}
+
+export default {
+  getHomeList: async () => {
+    return [
+      {
+        slug: 'originals',
+        title: 'Originais do Netflix',
+        items: await basicFetch('/discover/tv?with_network=213&'),
+      },
+      {
+        slug: 'trending',
+        title: 'Recomendados para Você',
+        items: await basicFetch('/trending/all/week?'),
+      },
+      {
+        slug: 'toprated',
+        title: 'Em Alta',
+        items: await basicFetch('/movie/top_rated?'),
+      },
+      {
+        slug: 'action',
+        title: 'Ação',
+        items: await basicFetch('/discover/movie?with_genres=28&'),
+      },
+      {
+        slug: 'comedy',
+        title: 'Terror',
+        items: await basicFetch('/discover/movie?with_genres=35&'),
+      },
+      {
+        slug: 'horror',
+        title: 'Terror',
+        items: await basicFetch('/discover/movie?with_genres=27&'),
+      },
+      {
+        slug: 'romance',
+        title: 'Romance',
+        items: await basicFetch('/discover/movie?with_genres=10749&'),
+      },
+      {
+        slug: 'documentary',
+        title: 'Documentários',
+        items: await basicFetch('/discover/movie?with_genres=99&'),
+      },
+    ]
+  },
+}
